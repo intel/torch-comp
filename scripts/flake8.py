8f55cdc04ea77d71a2b98a38ec4dee459752e29c
Liao, Wei <wei.liao@intel.com>
2025-07-18 10:01:24 +0800

    update flake8 scripts

---
+++ scripts/flake8.py
@@ -0,0 +1,71 @@
+import os
+import shutil
+import subprocess
+import sys
+
+_IS_WINDOWS = sys.platform == "win32"
+
+def check_flake8_errors(base_dir, filepath):
+    if shutil.which("flake8") is None:
+        return -1
+    flak8_cmd = ["flake8"]  # '--quiet'
+
+    if shutil.which("black") is None:
+        return -1
+    black_cmd = ["black"]
+
+    if os.path.isdir(filepath):
+        for root, dirs, files in os.walk(filepath):
+            for file in files:
+                if file.endswith(".py"):
+                    black_cmd.append(os.path.join(root, file))
+                    flak8_cmd.append(os.path.join(root, file))
+    elif os.path.isfile(filepath):
+        black_cmd.append(filepath)
+        flak8_cmd.append(filepath)
+
+    # Auto format python code.
+    blk_output = subprocess.check_output(
+        black_cmd,
+        cwd=base_dir,
+        stderr=subprocess.STDOUT,
+    )
+    output_string = blk_output.decode("utf-8")
+    print(output_string)
+    if output_string.find("reformatted") == -1:
+        ret_blk = 0
+    else:
+        ret_blk = 1
+
+    # Check code style.
+    ret_flak8 = subprocess.call(flak8_cmd, cwd=base_dir)
+    status_code = ret_flak8 + ret_blk
+    print("status code: ", status_code)
+
+    return status_code
+
+
+if __name__ == "__main__":
+    if _IS_WINDOWS:
+        print("skip flake8 check for Windows")
+        sys.exit(0)
+
+    base_dir = os.path.abspath(
+        os.path.dirname(os.path.join(os.path.abspath(__file__), "../../"))
+    )
+    setupfile = os.path.join(base_dir, "setup.py")
+    base_pydir = os.path.join(base_dir, "src/torch_comp")
+    base_scripts = os.path.join(base_dir, "scripts")
+    base_test = os.path.join(base_dir, "test")
+
+    Check_dir = [setupfile, base_pydir, base_scripts, base_test]
+    ret = sum([check_flake8_errors(base_dir, path) for path in Check_dir])
+    if ret > 0:
+        print("ERROR: flake8 found format errors!")
+        sys.exit(1)
+    elif ret < 0:
+        print(
+            "WARNING: Please check format!"
+        )        
+    else:
+        print("Pass!")
